d8a3efbb94a535321f1c43fe5daf1864ef51bb8a
ThaN1eL <yuiharabusiness@gmail.com>
2025-03-27 22:04:49 +0800

    setting up reactapp

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import NavBar from './components/NavBar';
+
+import HomePage from './pages/HomePage';
+import ProductPage from './pages/ProductPage';
+
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <h1 className='text-red-600'> HEYOO </h1>
+    <div className="min-h-screen bg-base-200 transition-colors duration-300">
+      <NavBar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product/:id" element={<ProductPage />} />
+      </Routes>
     </div>
   )
 }
