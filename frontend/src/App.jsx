3d7f457f90fe38041e0d7b62c315672112c3e8c5
ThaN1eL <yuiharabusiness@gmail.com>
2025-03-28 22:29:03 +0800

    navbard export data-theme unsolved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="min-h-screen bg-base-200 transition-colors duration-300">
+    <div className="min-h-screen bg-base-200 transition-colors duration-300" data-theme="forest">
       <NavBar />
 
       <Routes>
@@ -15,7 +15,7 @@ function App() {
         <Route path="/product/:id" element={<ProductPage />} />
       </Routes>
     </div>
-  )
+  );
 }
 
 export default App
